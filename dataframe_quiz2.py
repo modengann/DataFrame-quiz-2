27e780f8ebd8e3d470f84e79aecaeee3ba6d6c69
CaseyGann <cmoden@gannacademy.org>
2023-10-31 09:07:43 -0400

    update

--- dataframe_quiz2.py
+++ dataframe_quiz2.py
@@ -12,7 +12,7 @@ def get_phil_info(df):
 def get_age_country(df): 
     pass
 
-def get_age_salary_of_last_three(df):
+def get_age_salary_of_middle_three(df):
     pass
 
 def get_role_megan_oscar(df):
@@ -25,7 +25,7 @@ def main():
     print("\nFifth Entry:\n", get_fifth_entry(df))
     print("\nPhil's Info:\n", get_phil_info(df))
     print("\nAge and Country Columns:\n", get_age_country(df))
-    print("\nAge and Salary of Last Three:\n", get_age_salary_of_last_three(df))
+    print("\nAge and Salary of Middle Three:\n", get_age_salary_of_middle_three(df))
     print("\nRole of Megan and Oscar:\n", get_role_megan_oscar(df))
 
 if __name__ == "__main__":
