c93c40893bebd4a68c293e1ec657b5d0c4ffc1ec
CaseyGann <cmoden@gannacademy.org>
2023-10-31 08:45:11 -0400

    init

---
+++ dataframe_quiz2.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def get_role(df):     
+    pass
+
+def get_fifth_entry(df):       
+    pass
+
+def get_phil_info(df):    
+    pass
+
+def get_age_country(df): 
+    pass
+
+def get_age_salary_of_last_three(df):
+    pass
+
+def get_role_megan_oscar(df):
+    pass
+
+def main():
+    df = pd.read_csv("data.csv")
+    print(get_role(df))
+
+if __name__ == "__main__":
+    main()
