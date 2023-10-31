01d7683e1b4d8304d8a0a174cb9d60e1ca480b40
CaseyGann <cmoden@gannacademy.org>
2023-10-31 08:49:29 -0400

    fixed

--- dataframe_quiz2.py
+++ dataframe_quiz2.py
@@ -19,8 +19,14 @@ def get_role_megan_oscar(df):
     pass
 
 def main():
-    df = pd.read_csv("data.csv")
-    print(get_role(df))
+    df = pd.read_csv("data.csv", index_col= 0)
+
+    print("Role Column:\n", get_role(df))
+    print("\nFifth Entry:\n", get_fifth_entry(df))
+    print("\nPhil's Info:\n", get_phil_info(df))
+    print("\nAge and Country Columns:\n", get_age_country(df))
+    print("\nAge and Salary of Last Three:\n", get_age_salary_of_last_three(df))
+    print("\nRole of Megan and Oscar:\n", get_role_megan_oscar(df))
 
 if __name__ == "__main__":
     main()
